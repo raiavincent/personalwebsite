a27c8b5acbcb6453ab20f977eb9a5ddd8978264f
Vincent Raia <vincentraia1@gmail.com>
2021-12-17 11:41:06 -0500

    sched passage

--- dailyMeditations.py
+++ dailyMeditations.py
@@ -1,11 +1,16 @@
 import random
+import schedule
+import time
 
 text = 'meditations.txt' # Declaring meditations as our text here
 
-with open(text,'r', encoding="utf-8") as myFile: # This will open the text as myFile, declares data as reading the text
-    data = myFile.read()
+def quoteWriter():
+    with open(text,'r', encoding="utf-8") as myFile: # This will open the text as myFile, declares data as reading the text
+        data = myFile.read()
 
-passage = random.choice(list(data.split(' \n')))
+    passage = random.choice(list(data.split(' \n')))
 
-with open('passage.txt', 'w') as f:
-    f.write(passage)
+    with open('passage.txt', 'w') as f:
+        f.write(passage)
+
+schedule.every().day.at("08:00").do(quoteWriter)
